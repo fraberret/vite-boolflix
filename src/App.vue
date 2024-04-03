a74767a5cb70fa62acfe87242001b324a3b1c1df
fraberret <f.berretta02@gmail.com>
2024-04-03 12:40:01 +0200

    add h3 on title

--- src/App.vue
+++ src/App.vue
@@ -36,10 +36,12 @@ export default {
 
     <div class="results">
         <ul v-for="film in films">
-            <li>{{ film.title }}</li>
-            <li>{{ film.original_title }}</li>
-            <li>{{ film.original_language }}</li>
-            <li>{{ film.vote_average }}</li>
+            <li>
+                <h3>{{ film.title }}</h3>
+            </li>
+            <li>Titolo Originale: {{ film.original_title }}</li>
+            <li>Lingua: {{ film.original_language }}</li>
+            <li>Voto: {{ film.vote_average }}</li>
 
         </ul>
     </div>
