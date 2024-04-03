bf3008f5e66ccd36edb3c29f0bb9bb34c31da9a5
fraberret <f.berretta02@gmail.com>
2024-04-03 12:28:46 +0200

    create a call api methods and use it in searchfilms method

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script>
-
+import axios from 'axios';
 
 export default {
     name: 'App',
     data() {
         return {
+            films: [],
             searchText: ''
         }
     },
     methods: {
+        callApi(url) {
+            axios.get(url)
+                .then(resp => {
+                    this.films = resp.data.results
+                })
+
+
+
+
+        },
         searchFilms() {
             console.log(this.searchText);
+            this.callApi(`https://api.themoviedb.org/3/search/movie?api_key=b1003d70cc2d6eaae13e67d404d98fdd&query=${this.searchText}`)
         }
     }
 }
