ddcda0397d939247040f9b924485aebe14b22018
fraberret <f.berretta02@gmail.com>
2024-04-03 11:25:21 +0200

    create a state file and import in the App to try

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script>
+import { state } from './state'
 
+export default {
+    name: 'App',
+    data() {
+        return {
+            state
+        }
+    }
+}
 </script>
 
 <template>
-
+    <h1>{{ state.message }}</h1>
 </template>
 
 <style></style>
---
+++ src/state.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+import { reactive } from 'vue'
+
+export const state = reactive({
+    message: 'hello'
+})
