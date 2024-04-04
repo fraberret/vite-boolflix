84a43df07e3bc7cbf14988e0b1d0ede8cfdd3d75
fraberret <f.berretta02@gmail.com>
2024-04-04 11:30:02 +0200

    create a serch tv series function

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,17 @@ export default {
         callApi(url) {
             axios.get(url)
                 .then(resp => {
-                    this.films = resp.data.results
+                    this.films = this.films.concat(resp.data.results)
                 })
 
         },
         searchFilms() {
             console.log(this.searchText);
             this.callApi(`https://api.themoviedb.org/3/search/movie?api_key=b1003d70cc2d6eaae13e67d404d98fdd&query=${this.searchText}`)
+        },
+        searchTvSeries() {
+            console.log(this.searchText);
+            this.callApi(`https://api.themoviedb.org/3/search/tv?api_key=b1003d70cc2d6eaae13e67d404d98fdd&language=it_IT&query=${this.searchText}`)
         }
     }
 }
@@ -33,11 +37,11 @@ export default {
 <template>
     <div class="search_bar">
         <input type="text" v-model="searchText" @keyup.enter="searchFilms">
-        <button @click="searchFilms">Search</button>
+        <button @click="searchFilms(); searchTvSeries()">Search</button>
     </div>
 
     <div class="results">
-        <ul v-for="film in films">
+        <ul v-for=" film  in  films ">
             <li>
                 <h3>{{ film.title }}</h3>
             </li>
