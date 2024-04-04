c3763cc4a58b3918ecf1791dc787cbf56a90790d
fraberret <f.berretta02@gmail.com>
2024-04-04 11:44:55 +0200

    change array's name

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
     },
     data() {
         return {
-            films: [],
+            allContents: [],
             searchText: ''
         }
     },
@@ -19,17 +19,17 @@ export default {
             axios.get(url)
                 .then(resp => {
 
-                    this.films = this.films.concat(resp.data.results)
+                    this.allContents = this.allContents.concat(resp.data.results)
                 })
 
         },
         searchFilms() {
-            this.films = []
+            this.allContents = []
             console.log(this.searchText);
             this.callApi(`https://api.themoviedb.org/3/search/movie?api_key=b1003d70cc2d6eaae13e67d404d98fdd&query=${this.searchText}`)
         },
         searchTvSeries() {
-            this.films = []
+            this.allContents = []
             console.log(this.searchText);
             this.callApi(`https://api.themoviedb.org/3/search/tv?api_key=b1003d70cc2d6eaae13e67d404d98fdd&language=it_IT&query=${this.searchText}`)
         }
@@ -39,22 +39,22 @@ export default {
 
 <template>
     <div class="search_bar">
-        <input type="text" v-model="searchText" @keyup.enter="searchFilms">
+        <input type="text" v-model="searchText" @keyup.enter="searchFilms(); searchTvSeries()">
         <button @click="searchFilms(); searchTvSeries()">Search</button>
     </div>
 
     <div class="results">
-        <ul v-for=" film  in  films ">
+        <ul v-for=" content  in  allContents ">
             <li>
-                <h3>{{ film.title }}</h3>
-                <h3>{{ film.name }}</h3>
+                <h3>{{ content.title }}</h3>
+                <h3>{{ content.name }}</h3>
 
             </li>
-            <li>Titolo Originale: {{ film.original_title }}</li>
+            <li>Titolo Originale: {{ content.original_title }}</li>
             <li>Lingua:
-                <Flags :language="film.original_language" />
+                <Flags :language="content.original_language" />
             </li>
-            <li>Voto: {{ film.vote_average }}</li>
+            <li>Voto: {{ content.vote_average }}</li>
 
         </ul>
     </div>
