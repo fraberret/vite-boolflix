b20f13ffc70197fb861f0f563e41ecd8e517bfe0
fraberret <f.berretta02@gmail.com>
2024-04-05 16:12:18 +0200

    add comments

--- src/App.vue
+++ src/App.vue
@@ -14,60 +14,58 @@ export default {
         return {
             allContents: [],
             searchText: '',
-            Ids: []
         }
     },
     methods: {
 
-
-
+        /* Funzione che riceve il testo di ricerca e il trigger */
         performSearch(searchText) {
 
+            /* Svuota allContents ad ogni ricerca */
             this.allContents = [],
-                this.searchText = searchText;
-            this.searchFilms();
-            this.searchTvSeries();
 
+                /* Passa il testo da ricercare */
+                this.searchText = searchText;
+
+            /* Invoca la funziona per cercare i film */
+            this.searchFilms();
+
+            /* Invoca la funziona per cercare le Serie Tv */
+            this.searchTvSeries();
         },
 
+        /* Funzione per fare le chiamate Ajax */
         callApi(url) {
             axios.get(url)
                 .then(resp => {
 
+                    /* Unisce gli array di Film e Serie Tv */
                     this.allContents = this.allContents.concat(resp.data.results)
-                    console.log(this.allContents);
-
-                    this.Ids = [];
-
-                    this.allContents.forEach(content => {
-                        this.Ids.push(content.id);
-                    })
-                    console.log(this.Ids);
                 })
 
         },
-
+        /* Funzione per richiamare i film */
         searchFilms() {
             console.log(this.searchText);
+
+            /* Chiamata Ajax per recuperare i film cercati */
             this.callApi(`https://api.themoviedb.org/3/search/movie?api_key=b1003d70cc2d6eaae13e67d404d98fdd&query=${this.searchText}`)
 
         },
+
+        /* Funzione per richiamare le serie Tv */
         searchTvSeries() {
             console.log(this.searchText);
+
+            /* Chiamata Ajax per recuperare le serie cercate */
             this.callApi(`https://api.themoviedb.org/3/search/tv?api_key=b1003d70cc2d6eaae13e67d404d98fdd&language=it_IT&query=${this.searchText}`)
         },
-
-        cast() {
-            this.Ids.forEach(id => {
-
-            })
-        }
-
 
     },
     mounted() {
 
         console.log(this.searchText);
+        /* Chiamata Ajax per far comparire da subito i film in pagina */
         this.callApi(`https://api.themoviedb.org/3/discover/movie?api_key=b1003d70cc2d6eaae13e67d404d98fdd`)
 
 
