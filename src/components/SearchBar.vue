522c15865de96735eae3f1aa3b3d823e2b51fdd9
fraberret <f.berretta02@gmail.com>
2024-04-05 11:37:14 +0200

    add style on searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -20,8 +20,9 @@ export default {
 
 <template>
     <div class="search_bar">
-        <input class="form-control me-2" type="text" v-model="searchText" @keyup.enter="search">
-        <button class="btn btn-outline-danger my-2 my-sm-0" @click="search">Search</button>
+        <input class="form-control me-2 bg-black rounded-0 text-white" type="text" v-model="searchText"
+            @keyup.enter="search" placeholder="Cerca">
+        <button class="btn btn-outline-danger my-2 my-sm-0 rounded-0" @click="search">Cerca</button>
     </div>
 </template>
 
@@ -31,5 +32,10 @@ export default {
     position: absolute;
     right: 0;
     display: flex;
+
+}
+
+input::-webkit-input-placeholder {
+    color: rgba(255, 255, 255, 0.584) !important;
 }
 </style>
